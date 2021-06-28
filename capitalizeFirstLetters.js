82a93f8b1112ec89e3c7736d8b63747bd3d877c9
Pits111 <isabel.mehlmann@gmail.com>
2021-06-28 16:53:00 +0200

    running tests

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,7 +1,4 @@
-// capitalizeFirst.js
 const assert = require('assert');
-
-// WRITE THE ACTUAL FUNCTION HERE
 
 const capitalizeFirstLetters = (input) => {
   if( input.length < 0 ) {
@@ -11,25 +8,18 @@ const capitalizeFirstLetters = (input) => {
   return input.split(" ").map(function(word){
     return word.charAt(0).toUpperCase() + word.slice(1);
   }).join(" ");
-
 }
 
 console.log(capitalizeFirstLetters("i am learning TDD"))
 
-// // Check that capitalizeFirstLetters is a function
 assert.strictEqual(typeof capitalizeFirstLetters, 'function');
 
-// // Check that capitalizeFirstLetters accepts one argument
 assert.strictEqual(capitalizeFirstLetters.length, 1);
 
-// // Check that capitalizeFirstLetters transforms javaScript correctly
 assert.strictEqual(capitalizeFirstLetters('goHome'), 'GoHome');
 
-// // Check that capitalizeFirstLetters transforms multiple words correctly
 assert.strictEqual(capitalizeFirstLetters('i am learning TDD'), 'I Am Learning TDD');
 
-// // Check that it works for a 1-character string
 assert.strictEqual(capitalizeFirstLetters('b'), 'B');
 
-// // Check that it works for an empty string
 assert.strictEqual(capitalizeFirstLetters(''), '');
