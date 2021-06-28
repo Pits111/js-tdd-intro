8bf71379aaa50cbc0ab9c1582915e11d53b85661
Pits111 <isabel.mehlmann@gmail.com>
2021-06-28 16:50:03 +0200

    running tests

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -23,13 +23,13 @@ assert.strictEqual(typeof capitalizeFirstLetters, 'function');
 assert.strictEqual(capitalizeFirstLetters.length, 1);
 
 // // Check that capitalizeFirstLetters transforms javaScript correctly
-assert.strictEqual(capitalizeFirstLetters('live is good'), 'Live Is Good');
+assert.strictEqual(capitalizeFirstLetters('goHome'), 'GoHome');
 
 // // Check that capitalizeFirstLetters transforms multiple words correctly
 assert.strictEqual(capitalizeFirstLetters('i am learning TDD'), 'I Am Learning TDD');
 
 // // Check that it works for a 1-character string
-assert.strictEqual(capitalizeFirstLetters('is this working'), 'Is This Working');
+assert.strictEqual(capitalizeFirstLetters('b'), 'B');
 
 // // Check that it works for an empty string
 assert.strictEqual(capitalizeFirstLetters(''), '');
